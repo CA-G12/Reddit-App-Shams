d2f6a55e3a559f8d6b68d45e86705ddd7a321cae
shamskhodary <shams.khodary@gmail.com>
2022-09-05 01:59:58 +0300

    finish the whole signup fucntion, handle errors, and edit on the email validation relates #21

--- server/controllers/register/signup.js
+++ server/controllers/register/signup.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const { signUpQuery, selectUserByEmail } = require('../../database/queries');
 const validate = require('../../validation/validator');
 const signupSchema = require('../../validation/sign-up.validate');
+const GenericError = require('../../helpers/GenericError');
 
 const signUp = (req, res) => {
   const {
@@ -12,15 +13,24 @@ const signUp = (req, res) => {
   validate(signupSchema, {
     username, email, password, confirm_password, image_url,
   }).then(() => selectUserByEmail(email))
-    .then((data) => res.send(data))
-
-    .then(() => bcrypt.hash(password, 10).then((hashed) => signUpQuery({
-      username, email, hashed, image_url,
-    })))
-    .then((userData) => console.log(userData))
-    .then((data) => res.json(data))
-    .catch((error) => res.status(error.status || 500)
-      .json({ error: error.msg || 'Something Went Wrong' }));
+    .then((data) => {
+      if (data.rowCount) {
+        const { username, email } = data.rows[0];
+        if (username === req.body.username) throw new GenericError(400, 'Username is used before')
+        if (email === req.body.email) throw new GenericError(400, 'Email already exists');
+      } else {
+        return bcrypt.hash(password, 10);
+      }
+    }).then((hashed) => signUpQuery({
+      username, email, password: hashed, image_url,
+    })).then((userData) => res.send(userData))
+    .catch((error) => {
+      if (error.name === 'ValidationError') res.status(400).json({
+        status: 400,
+        msg: error.message
+      });
+      return res.status(500).json(error);
+    });
 };
 
 module.exports = signUp;
--- server/validation/sign-up.validate.js
+++ server/validation/sign-up.validate.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const signupSchema = joi.object({
   username: joi.string().min(3).max(30)
     .required(),
-  email: joi.string().required(),
+  email: joi.string().min(8).max(100).email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
   password: joi.string().alphanum().required(),
   confirm_password: joi.ref('password'),
   image_url: joi.string().required(),
