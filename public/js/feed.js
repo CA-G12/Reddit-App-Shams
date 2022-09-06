45840fb2cda01f25bc5bb3516b155d17c24cd483
shamskhodary <shams.khodary@gmail.com>
2022-09-07 01:13:24 +0300

    added profile query, function for the feed page in addition to fetching the data and displaying the user info in the protected page, then fetch all the posts relates #33

--- public/js/feed.js
+++ public/js/feed.js
@@ -10,6 +10,105 @@ const logout = () => {
     }).catch(err => console.log(err));
 }
 
-
-
 logoutBtn.addEventListener('click', logout);
+
+
+fetch('/user/profile').then(data => data.json()).then(result => result.data).then(userData => profileInfo(userData))
+
+function profileInfo(userData) {
+
+  const username = document.querySelector('.users p');
+  username.textContent = userData['username'];
+
+  const userImg = document.querySelector('.icons img');
+  const accountImg = document.querySelector('.account-img img');
+  userImg.src = userData['image_url'];
+  accountImg.src = userData['image_url'];
+}
+
+fetch('/home').then(data => data.json()).then(response => handleDom(response));
+
+function handleDom(response) {
+  response.reverse().forEach((ele) => {
+    const mainContainer = document.querySelector('#posts .container')
+    const bigDiv = document.createElement('div');
+    bigDiv.className = 'big';
+    mainContainer.appendChild(bigDiv);
+
+    const votes = document.createElement('div');
+    votes.className = 'votes';
+    bigDiv.appendChild(votes);
+
+    const upVote = document.createElement('i');
+    upVote.className = 'fa-solid fa-up-long';
+    votes.appendChild(upVote);
+
+    const votesNum = document.createElement('p');
+    votesNum.textContent = '14.5k';
+    votes.appendChild(votesNum);
+
+    const downVote = document.createElement('i');
+    downVote.className = 'fa-solid fa-down-long';
+    votes.appendChild(downVote);
+
+    const postDiv = document.createElement('div');
+    postDiv.className = 'post';
+    bigDiv.appendChild(postDiv);
+
+    const postPublisher = document.createElement('div');
+    postPublisher.className = 'post-publisher';
+    postDiv.appendChild(postPublisher);
+
+    const userImage = document.createElement('img');
+    userImage.src = ele['image_url'];
+    postPublisher.appendChild(userImage);
+
+    const publisherInfo = document.createElement('div');
+    publisherInfo.className = 'publisher-info';
+    postPublisher.appendChild(publisherInfo);
+
+    const userName = document.createElement('p');
+    userName.textContent = ele['username'];
+    publisherInfo.appendChild(userName);
+
+    const event = new Date(ele['post_date']);
+    const postDate = document.createElement('span');
+    postDate.textContent = `Posted in ${event.toLocaleDateString('es-CL')}`;
+    publisherInfo.appendChild(postDate);
+
+    const ellipsis = document.createElement('i');
+    ellipsis.className = 'fa-solid fa-ellipsis';
+    postPublisher.appendChild(ellipsis);
+
+    const content = document.createElement('p');
+    content.className = 'content';
+    content.textContent = ele['content']
+    postDiv.appendChild(content);
+
+    const postImgDiv = document.createElement('div');
+    postImgDiv.className = 'post-img';
+    postDiv.appendChild(postImgDiv);
+
+    const postImg = document.createElement('img');
+    postImg.src = ele['post_image'];
+    postImgDiv.appendChild(postImg);
+
+    const social = document.createElement('div');
+    social.className = 'social'
+    postDiv.appendChild(social);
+
+    const comment = document.createElement('p');
+    comment.innerHTML = `<i class="fa-regular fa-message"></i> Comment`;
+    const share = document.createElement('p');
+    share.innerHTML = `<i class="fa-regular fa-share-from-square"></i> Share`;
+    const Save = document.createElement('p');
+    Save.innerHTML = `<i class="fa-regular fa-bookmark"></i> Save`;
+    social.appendChild(comment);
+    social.appendChild(share);
+    social.appendChild(Save);
+  })
+}
+
+
+
+
--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -5,6 +5,7 @@ const getUserPostsQuery = require('./posts/getUserPostsQuery');
 const selectUserByEmail = require('./register/selectUserByEmail');
 const signInQuery = require('./register/siginQuery');
 const signUpQuery = require('./register/signupQuery');
+const userProfileQuery = require('./user/profileQuery');
 
 
-module.exports = { signUpQuery, selectUserByEmail, signInQuery, showPostsQuery, addPostQuery, deletePostQuery, getUserPostsQuery };
+module.exports = { signUpQuery, selectUserByEmail, signInQuery, showPostsQuery, addPostQuery, deletePostQuery, getUserPostsQuery, userProfileQuery };
